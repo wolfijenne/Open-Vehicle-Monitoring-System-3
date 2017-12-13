4bde20191ac7d88516651f9ecb97f1028af5f37a
Wolfgang Jenne <wolfi@spider.li>
2017-12-14 00:33:15 +0100

    Implementation of SOC

--- vehicle/OVMS.V3/components/vehicle_renaultzoe/src/vehicle_renaultzoe.cpp
+++ vehicle/OVMS.V3/components/vehicle_renaultzoe/src/vehicle_renaultzoe.cpp
@@ -32,18 +32,233 @@
 static const char *TAG = "v-renaultzoe";
 
 #include <stdio.h>
+#include "pcp.h"
 #include "vehicle_renaultzoe.h"
+#include "ovms_metrics.h"
+#include "metrics_standard.h"
 
+#define VERSION "0.1.0"
+// Polling types ("modes") supported: see vehicle.h
+//
+// polltime[state] = poll period in seconds, i.e. 10 = poll every 10 seconds
+//
+// state: 0..2; set by vehicle_poll_setstate()
+//     0=off, 1=on, 2=charging
+//
+// Use broadcasts to query all ECUs. The request is sent to ID 0x7df (broadcast
+// listener) and the poller accepts any response ID from 0x7e8..0x7ef.
+// The moduleid is not used for broadcasts, just needs to be != 0.
+//
+// To poll a specific ECU, send to its specific ID (one of 0x7e0..0x7e7).
+// ECUs will respond using their specific ID plus 8 (= range 0x7e8..0x7ef).
+
+static const OvmsVehicle::poll_pid_t vehicle_renaultzoe_polls[] =
+{
+{ 0x7e4, 0x7ec, VEHICLE_POLL_TYPE_OBDIIEXTENDED, 0x2002, { 10, 10, 10 } },  // SOC
+//{ 0x7e4, 0x7ec, VEHICLE_POLL_TYPE_OBDIIEXTENDED, 0x2006, {  10, 10,  10 } },  // Odometer
+//{ 0x7e4, 0x7ec, VEHICLE_POLL_TYPE_OBDIIEXTENDED, 0x3203, {  10, 10,  10 } },  // Battery Voltage
+//{ 0x7e4, 0x7ec, VEHICLE_POLL_TYPE_OBDIIEXTENDED, 0x3204, {  10, 10,  10 } },  // Battery Current
+//{ 0x75a, 0x77e, VEHICLE_POLL_TYPE_OBDIIEXTENDED, 0x3018, {  10, 10,  10 } },  // DCDC Temp
+//{ 0x7e4, 0x77e, VEHICLE_POLL_TYPE_OBDIIEXTENDED, 0x302b, {  10, 10,  10 } },  // Inverter Temp
+//{ 0x7e4, 0x7ec, VEHICLE_POLL_TYPE_OBDIIEXTENDED, 0x3028, {  10, 10,  10 } },  // 12Battery Current
+//{ 0x7e4, 0x7ec, VEHICLE_POLL_TYPE_OBDIIEXTENDED, 0x2005, {  10, 10,  10 } },  // 12Battery Voltage
+//{ 0x7e4, 0x7ec, VEHICLE_POLL_TYPE_OBDIIEXTENDED, 0x3206, {  10, 10,  10 } },  // Battery SOH
+//{ 0x7e4, 0x7ec, VEHICLE_POLL_TYPE_OBDIIEXTENDED, 0x33dc, {  10, 10,  10 } },  // Domnestic Engergy
+//{ 0x792, 0x793, VEHICLE_POLL_TYPE_OBDIIEXTENDED, 0x504A, {  10, 10,  10 } },  // Mains active Power
+//{ 0x792, 0x793, VEHICLE_POLL_TYPE_OBDIIEXTENDED, 0x5063, {  10, 10,  10 } },  // Charging State
+//{ 0x792, 0x793, VEHICLE_POLL_TYPE_OBDIIEXTENDED, 0x5062, {  10, 10,  10 } },  // Ground Resistance
+
+// -END-
+{ 0, 0, 0x00, 0x00, { 0, 0, 0 } }
+
+};
+// Start Insertion from Kia Soul
+
+// Init
 OvmsVehicleRenaultZoe::OvmsVehicleRenaultZoe()
   {
-  ESP_LOGI(TAG, "Generic Renault Zoe vehicle module");
+  ESP_LOGI(TAG, "Renault Zoe vehicle module");
+      StdMetrics.ms_v_bat_soc->SetValue(50.5);
+      memset( m_vin, 0, sizeof(m_vin));
+      memset( rz_battery_cell_voltage ,0, sizeof(rz_battery_cell_voltage));
+      memset( rz_battery_module_temp, 0, sizeof(rz_battery_module_temp));
+      memset( rz_tpms_id, 0, sizeof(rz_tpms_id));
+      
+      rz_obc_volt = 230;
+      rz_battery_current = 0;
+      
+      rz_battery_cum_charge_current = 0;
+      rz_battery_cum_discharge_current = 0;
+      rz_battery_cum_charge_power = 0;
+      rz_battery_cum_discharge_power = 0;
+      
+      rz_charge_bits.Charging = false;
+      rz_charge_bits.FanStatus = 0;
+      
+      rz_battery_min_temperature = 0;
+      rz_battery_max_temperature = 0;
+      
+      rz_heatsink_temperature = 0;
+      rz_battery_fan_feedback = 0;
+      
+      rz_send_can.id = 0;
+      rz_send_can.status = 0;
+      memset( rz_send_can.byte, 0, sizeof(rz_send_can.byte));
+      
+      rz_maxrange = CFG_DEFAULT_MAXRANGE;
+      
+      // C-Bus
+      RegisterCanBus(1, CAN_MODE_ACTIVE, CAN_SPEED_500KBPS);
+      
+      MyConfig.RegisterParam("x.rz", "Renault Zoe", true, true);
+      ConfigChanged(NULL);
+      
+      // init metrics:
+      m_version = MyMetrics.InitString("x.rz.m.version", 0, VERSION " " __DATE__ " " __TIME__);
+      m_b_cell_volt_max = MyMetrics.InitFloat("x.rz.m.b.cell.volt.max", 10, 0, Volts);
+      m_b_cell_volt_min = MyMetrics.InitFloat("x.rz.m.b.cell.volt.min", 10, 0, Volts);
+      m_c_power = MyMetrics.InitFloat("x.rz.m.c.power", 10, 0, kW);
+      
+      
+      PollSetPidList(m_can1,vehicle_renaultzoe_polls);
+      PollSetState(0);
   }
+
+// End Insertion from Kia Soul
 
 OvmsVehicleRenaultZoe::~OvmsVehicleRenaultZoe()
   {
   ESP_LOGI(TAG, "Shutdown Renault Zoe vehicle module");
   }
+// Start Insertion from Kia Soul
 
+/**
+ * ConfigChanged: reload single/all configuration variables
+ */
+
+void OvmsVehicleRenaultZoe::ConfigChanged(OvmsConfigParam* param)
+{
+    ESP_LOGD(TAG, "Renault Zoe reload configuration");
+    
+    // Instances:
+    // x.rz
+    //      batteryCapacity   Battery capacity in wH (Default: 270000)
+    //  suffsoc           Sufficient SOC [%] (Default: 0=disabled)
+    //  suffrange         Sufficient range [km] (Default: 0=disabled)
+    //  maxrange          Maximum ideal range at 20 °C [km] (Default: 160)
+    //
+    //  canwrite          Bool: CAN write enabled (Default: no)
+    //  autoreset         Bool: SEVCON reset on error (Default: yes)
+    //  kickdown          Bool: SEVCON automatic kickdown (Default: yes)
+    //  autopower         Bool: SEVCON automatic power level adjustment (Default: yes)
+    //  console           Bool: SimpleConsole inputs enabled (Default: no)
+    //
+    
+    rz_battery_capacity = (float)MyConfig.GetParamValueInt("x.rz", "batteryCapacity", CGF_DEFAULT_BATTERY_CAPACITY);
+    
+}
+
+////////////////////////////////////////////////////////////////////////
+// vehicle_renaultzoe_car_on()
+// Takes care of setting all the state appropriate when the car is on
+// or off. Centralized so we can more easily make on and off mirror
+// images.
+void OvmsVehicleRenaultZoe::vehicle_renaultzoe_car_on(bool isOn)
+{
+    
+    if (isOn && !StdMetrics.ms_v_env_on->AsBool())
+    {
+        // Car is ON
+        StdMetrics.ms_v_env_on->SetValue(isOn);
+        StdMetrics.ms_v_env_awake->SetValue(isOn);
+        
+        PollSetState(1);
+        //TODO net_req_notification(NET_NOTIFY_ENV);
+    }
+    else if(!isOn && StdMetrics.ms_v_env_on->AsBool())
+    {
+        // Car is OFF
+    }
+}
+
+/**
+ * Handles incoming CAN-frames on bus 1, the C-bus
+ */
+void OvmsVehicleRenaultZoe::IncomingFrameCan1(CAN_frame_t* p_frame)
+{
+    //ESP_LOGI(TAG, "Renault Zoe IncomingFrameCan1");
+    uint8_t *d = p_frame->data.u8;
+    
+    switch (p_frame->MsgID)
+    {
+        case 0x018:
+        {
+            // Doors status Byte 0:
+            StdMetrics.ms_v_door_chargeport->SetValue((d[0] & 0x01) > 0);
+            StdMetrics.ms_v_door_fl->SetValue((d[0] & 0x10) > 0);
+            StdMetrics.ms_v_door_fr->SetValue((d[0] & 0x80) > 0);
+            StdMetrics.ms_v_door_rl->SetValue(false); //TODO
+            StdMetrics.ms_v_door_rr->SetValue(false); //TODO
+            // Light status Byte 3 & 5
+            // Seat belt status Byte 7
+        }
+            break;
+            
+            
+        case 0x4b0:
+        {
+            // Motor RPM based on wheel rotation
+            int rpm = (d[0]+(d[1]<<8)) * 8.206;
+            StdMetrics.ms_v_mot_rpm->SetValue( rpm );
+        }
+            break;
+            
+    }
+}
+
+
+/**
+ * Incoming poll reply messages
+ */
+void OvmsVehicleRenaultZoe::IncomingPollReply(canbus* bus, uint16_t type, uint16_t pid, uint8_t* data, uint8_t length, uint16_t mlremain)
+{
+//    ESP_LOGI(TAG, "Renault Zoe IncomingPollReply");
+//    uint8_t bVal;
+//    UINT base;
+//    uint32_t lVal;
+
+    
+    switch (m_poll_moduleid_low) {
+            
+        case 0x7ec:
+            switch (pid) {
+                    
+                case 0x2002:
+                    StdMetrics.ms_v_bat_soc->SetValue( float(CAN_UINT(0)) * 2 / 100);
+                    break;
+
+            }
+    }
+}
+
+/**
+ * Ticker1: Called every second
+ */
+void OvmsVehicleRenaultZoe::Ticker1(uint32_t ticker)
+{
+}
+
+/**
+ *  Sets the charge metrics
+ */
+void OvmsVehicleRenaultZoe::SetChargeMetrics(float voltage, float current, float climit, bool chademo)
+{
+    StdMetrics.ms_v_charge_voltage->SetValue( voltage, Volts );
+    StdMetrics.ms_v_charge_current->SetValue( current, Amps );
+    StdMetrics.ms_v_charge_climit->SetValue( climit, Amps);
+}
+
+// End Insertion from Kia Soul
 class OvmsVehicleRenaultZoeInit
   {
   public: OvmsVehicleRenaultZoeInit();
--- vehicle/OVMS.V3/components/vehicle_renaultzoe/src/vehicle_renaultzoe.h
+++ vehicle/OVMS.V3/components/vehicle_renaultzoe/src/vehicle_renaultzoe.h
@@ -40,6 +40,136 @@ class OvmsVehicleRenaultZoe : public OvmsVehicle
   public:
     OvmsVehicleRenaultZoe();
     ~OvmsVehicleRenaultZoe();
-  };
+  
+
+public:
+void IncomingFrameCan1(CAN_frame_t* p_frame);
+void Ticker1(uint32_t ticker);
+void IncomingPollReply(canbus* bus, uint16_t type, uint16_t pid, uint8_t* data, uint8_t length, uint16_t mlremain);
+void ConfigChanged(OvmsConfigParam* param);
+
+
+
+protected:
+void vehicle_renaultzoe_car_on(bool isOn);
+void UpdateMaxRangeAndSOH(void);
+uint16_t calcMinutesRemaining(float target);
+bool SendCanMessage_sync(uint16_t id, uint8_t count,
+                         uint8_t serviceId, uint8_t b1, uint8_t b2, uint8_t b3, uint8_t b4,
+                         uint8_t b5, uint8_t b6);
+bool SendCommandInSessionMode(uint16_t id, uint8_t count,
+                              uint8_t serviceId, uint8_t b1, uint8_t b2, uint8_t b3, uint8_t b4,
+                              uint8_t b5, uint8_t b6 );
+bool SetTemporarySessionMode(uint16_t id, uint8_t mode);
+bool SetDoorLock(bool open, const char* password);
+bool OpenTrunk(const char* password);
+bool IsPasswordOk(const char *password);
+void SetChargeMetrics(float voltage, float current, float climit, bool chademo);
+
+// Kia Soul EV specific metrics
+OvmsMetricString* m_version;
+OvmsMetricFloat*  m_b_cell_volt_max;           // Battery cell maximum voltage
+OvmsMetricFloat*  m_b_cell_volt_min;           // Battery cell minimum voltage
+OvmsMetricFloat*  m_b_cell_det_max;               // Battery cell maximum detoriation
+OvmsMetricFloat*  m_b_cell_det_min;               // Battery cell minimum detoriation
+OvmsMetricFloat*  m_c_power;                            // Available charge power
+//float rz_battery_max_detoriation;                 //02 21 05 -> 24 1+2
+//float rz_battery_min_detoriation;                 //02 21 05 -> 24 4+5
+
+
+#define CFG_DEFAULT_MAXRANGE 160
+int rz_maxrange = CFG_DEFAULT_MAXRANGE;        // Configured max range at 20 °C
+
+#define CGF_DEFAULT_BATTERY_CAPACITY 27000
+float rz_battery_capacity = CGF_DEFAULT_BATTERY_CAPACITY; //TODO Detect battery capacity from VIN or number of batterycells
+
+char m_vin[18];
+
+uint32_t rz_tpms_id[4];
+float rz_obc_volt;
+
+float rz_trip_start_odo;
+float rz_last_soc;
+float rz_last_ideal_range;
+uint8_t rz_bms_soc;
+float rz_start_cdc;                     // Used to calculate trip power use (Cumulated discharge)
+float rz_start_cc;                      // Used to calculate trip recuperation (Cumulated charge)
+float rz_cum_charge_start;     // Used to calculate charged power.
+
+int8_t rz_battery_module_temp[12];
+
+INT rz_battery_current;                                 //Battery current               02 21 01 -> 21 7+22 1
+
+uint32_t rz_battery_cum_charge_current;         //Cumulated charge current    02 21 01 -> 24 6+7 & 25 1+2
+uint32_t rz_battery_cum_discharge_current;    //Cumulated discharge current 02 21 01 -> 25 3-6
+uint32_t rz_battery_cum_charge_power;                         //Cumulated charge power      02 21 01 -> 25 7 + 26 1-3
+uint32_t rz_battery_cum_discharge_power;                 //Cumulated discharge power   02 21 01 -> 26 4-7
+uint32_t rz_battery_cum_op_time;                     //Cumulated operating time    02 21 01 -> 27 1-4
+
+uint8_t rz_battery_cell_voltage[100];
+
+uint8_t rz_battery_min_temperature;             //02 21 05 -> 21 7
+uint8_t rz_battery_inlet_temperature;         //02 21 05 -> 21 6
+uint8_t rz_battery_max_temperature;             //02 21 05 -> 22 1
+uint8_t rz_battery_heat_1_temperature;     //02 21 05 -> 23 6
+uint8_t rz_battery_heat_2_temperature;     //02 21 05 -> 23 7
+
+
+uint8_t rz_heatsink_temperature; //TODO Remove?
+uint8_t rz_battery_fan_feedback;
+
+struct {
+    unsigned char ChargingChademo : 1;
+    unsigned char Charging : 1;
+    unsigned char : 1;
+    unsigned char : 1;
+    unsigned char FanStatus : 4;
+} rz_charge_bits;
+
+struct {
+    uint8_t byte[8];
+    uint8_t status;
+    uint16_t id;
+}  rz_send_can;
+
+const TickType_t xDelay = 50 / portTICK_PERIOD_MS;
+};
+
+#define SQR(n) ((n)*(n))
+#define ABS(n) (((n) < 0) ? -(n) : (n))
+#define MIN(n,m) ((n) < (m) ? (n) : (m))
+#define MAX(n,m) ((n) > (m) ? (n) : (m))
+#define LIMIT_MIN(n,lim) ((n) < (lim) ? (lim) : (n))
+#define LIMIT_MAX(n,lim) ((n) > (lim) ? (lim) : (n))
+
+// CAN buffer access macros: b=byte# 0..7 / n=nibble# 0..15
+#define CAN_BYTE(b)     data[b]
+#define CAN_UINT(b)     (((UINT)CAN_BYTE(b) << 8) | CAN_BYTE(b+1))
+#define CAN_UINT24(b)   (((uint32_t)CAN_BYTE(b) << 16) | ((UINT)CAN_BYTE(b+1) << 8) | CAN_BYTE(b+2))
+#define CAN_UINT32(b)   (((uint32_t)CAN_BYTE(b) << 24) | ((uint32_t)CAN_BYTE(b+1) << 16)  | ((UINT)CAN_BYTE(b+2) << 8) | CAN_BYTE(b+3))
+#define CAN_NIBL(b)     (can_databuffer[b] & 0x0f)
+#define CAN_NIBH(b)     (can_databuffer[b] >> 4)
+#define CAN_NIB(n)      (((n)&1) ? CAN_NIBL((n)>>1) : CAN_NIBH((n)>>1))
+
+#define TO_CELCIUS(n)    ((float)n-40)
+#define TO_PSI(n)        ((float)n/4.0)
+
+#define BAT_SOC            StdMetrics.ms_v_bat_soc->AsFloat(100)
+#define BAT_SOH            StdMetrics.ms_v_bat_soh->AsFloat(100)
+#define LIMIT_SOC        StdMetrics.ms_v_charge_limit_soc->AsFloat(0)
+#define LIMIT_RANGE        StdMetrics.ms_v_charge_limit_range->AsFloat(0, Kilometers)
+#define EST_RANGE        StdMetrics.ms_v_bat_range_est->AsFloat(100, Kilometers)
+#define IDEAL_RANGE        StdMetrics.ms_v_bat_range_ideal->AsFloat(100, Kilometers)
+#define FULL_RANGE        StdMetrics.ms_v_bat_range_full->AsFloat(160, Kilometers)
+#define POS_ODO            StdMetrics.ms_v_pos_odometer->AsFloat(0, Kilometers)
+#define CHARGE_CURRENT    StdMetrics.ms_v_charge_current->AsFloat(0, Amps)
+#define CHARGE_VOLTAGE    StdMetrics.ms_v_charge_voltage->AsFloat(0, Volts)
+#define SET_CHARGE_STATE(n)        StdMetrics.ms_v_charge_state->SetValue(n)
+
+#define VEHICLE_POLL_TYPE_OBDII_IOCTRL_BY_ID 0x2F // InputOutputControlByIdentifier
+
+#define SMART_JUNCTION_BOX 0x771
+#define BODY_CONTROL_MODULE  0x7A0
+
 
 #endif //#ifndef __VEHICLE_RENAULTZOE_H__
